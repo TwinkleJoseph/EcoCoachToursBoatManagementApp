d951290895e3b8e59146603ffe3f41f31dd52d0a
TwinkleJoseph <twinkleljoseph@gmail.com>
2023-02-06 02:09:00 -0800

    show boat statuses from api

--- web/src/components/Pool.js
+++ web/src/components/Pool.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState  } from 'react';
 import SwimLane from './SwimLane'
 
 const initialData = [
-  { id: 1, status: 'Docked', boats: [{ id: 1, name: 'Boat 1' }, { id: 2, name: 'Boat 2' }] },
-  { id: 2, status: 'Outbound to Sea', boats: [{ id: 3, name: 'Boat 3' }] },
-  { id: 3, status: 'Inbound to Harbor', boats: [{ id: 4, name: 'Boat 4' }] },
-  { id: 4, status: 'Maintenance', boats: [{ id: 5, name: 'Boat 5' }] }
+  { id: 1, status: 'Docked' },
+  { id: 2, status: 'Outbound to Sea' },
+  { id: 3, status: 'Inbound to Harbor' },
+  { id: 4, status: 'Maintenance' }
 
 ];
 
 const Pool = () => {
+    
     const [data, setData] = useState(initialData);  
     return (
       <div style={{ display: 'flex' }}>
         {data.map(lane => (
-          <SwimLane key={lane.id} status={lane.status} boats={lane.boats} />
+          <SwimLane key={lane.id} status={lane.status}/>
         ))}
       </div>
     );
--- web/src/components/SwimLane.js
+++ web/src/components/SwimLane.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
 import "../styles/App.css";
 
-const SwimLane = ({ status, boats }) => {
+const SwimLane = ({ status }) => {
+    
+    const [boats, setBoats] = useState([]); 
+    useEffect(() => {
+      const url = "http://localhost:8000/search?status="+status
+      const fetchData = async () => {
+        try {
+          const response = await fetch(url);
+          const json = await response.json();
+          console.log(json);
+          setBoats(json.boats)
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+
+      fetchData();
+    }, []);    
     return (
       <div style={{ marginRight: 8 }}>
         <h3>{status}</h3>
         {boats.map(boat => (
           <div key={boat.id} style={{ marginBottom: 8, padding: 8, backgroundColor: '#f2f2f2' }}>
-            {boat.name}
+            {boat.vesselName}
           </div>
         ))}
       </div>
