989a9114ef42d4fe0ff9e35a448a5ba79eb905f7
TwinkleJoseph <twinkleljoseph@gmail.com>
2023-02-05 22:51:42 -0800

    react web app initial draft

---
+++ web/src/components/App.js
@@ -0,0 +1,15 @@
+import "../styles/App.css";
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Header">Echo Tours Boat Tracking Application</div>
+      </div>
+    );
+  }
+}
+
+export default App;
