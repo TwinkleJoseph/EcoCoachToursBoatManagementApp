ac94ac1fb5ad514e08ce9c373e7fb6bf367c7373
TwinkleJoseph <twinkleljoseph@gmail.com>
2023-02-07 01:45:16 -0800

    database scripts

---
+++ db/boat_guide_dml.sql
@@ -0,0 +1,37 @@
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Bob', 'Squirrel', '134558', '250-578-1900', '587 Yate St');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Adam', 'Gillchrist', '2345566', '250-578-1901', '435 Coutney St');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Daisy', 'Jeroms', '456666', '250-578-1902', '123 Kings St');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Jaun', 'Felix', '4457878', '250-578-1903', '321 Pandora Ave');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Alicia', 'Duglas', '1234566', '250-578-1904', '234 Wale Rd');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'James', 'Harper', '44566777', '250-578-1905', '2307 Cloverdale Ave');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Peter', 'Sheraton', '2334566', '250-578-1906', '456 Johnson St');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Emily', 'Matthews', '7779995', '250-578-1907', '255 Govt St');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Adam', 'Peters', '1113234', '250-578-1908', '556 Kimberly St');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Thomas', 'Paul', '22346566', '250-578-1909', '677 Gamble St');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Ali', 'Ibrahim', '23465677', '250-578-1910', '4567 Maple Leaf St');
+INSERT INTO public.guides(
+	id, first_name, last_name, license_number, phone, address)
+	VALUES (uuid_generate_v4(),'Juan', 'Young', '888989446', '250-578-1911', '444 Vancouver St');	
+commit;	
--- db/boat_tracker_db_ddl.sql
+++ db/boat_tracker_db_ddl.sql
@@ -13,9 +13,26 @@ CREATE TABLE IF NOT EXISTS public.fishing_boats
     status character varying(40) COLLATE pg_catalog."default",
     created_datetime timestamp without time zone NOT NULL DEFAULT now(),
     updated_datetime timestamp without time zone NOT NULL DEFAULT now(),
-    CONSTRAINT delivery_trucks_pkey PRIMARY KEY (id)
+    CONSTRAINT fishing_boats_pkey PRIMARY KEY (id)
 );
 
 
 ALTER TABLE public.fishing_boats
     OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.guides
+(
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    first_name character varying(50) COLLATE pg_catalog."default",
+    last_name character varying(50) COLLATE pg_catalog."default",
+    phone character varying(50) COLLATE pg_catalog."default",
+    address character varying(50) COLLATE pg_catalog."default",
+    license_number character varying(50) COLLATE pg_catalog."default",
+    created_datetime timestamp without time zone NOT NULL DEFAULT now(),
+    updated_datetime timestamp without time zone NOT NULL DEFAULT now(),
+    CONSTRAINT guides_pkey PRIMARY KEY (id)
+);
+
+
+ALTER TABLE public.guides
+    OWNER to postgres;    
